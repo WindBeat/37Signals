8579d311e511676fd8c157cffec1ad20809d5963
WindBeat <jordanwalker465@gmail.com>
2014-01-01 16:55:31 -0800

    Added Javascript
    Added javascript, edited html and css, and added new images.

---
+++ application.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+	$('.box1').hover(function() {
+		$('#core').toggleClass('hide');
+		$('#base').toggleClass('hide');
+		$('#barrow').toggleClass('hide');
+	});
+});
+
+$(document).ready(function() {
+	$('.box2').hover(function() {
+		$('#core').toggleClass('hide');
+		$('#high').toggleClass('hide');
+		$('#harrow').toggleClass('hide');
+	});
+});		
+
+$(document).ready(function() {
+	$('.box3').hover(function() {
+		$('#core').toggleClass('hide');
+		$('#camp').toggleClass('hide');
+		$('#carrow').toggleClass('hide');
+	});
+});
